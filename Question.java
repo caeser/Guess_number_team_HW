b9f28b446d719d52378b805a11c6350567fc2243
廉強 <xsoameix@gmail.com>
2012-10-21 17:47:15 +0800

    調用出題函數的主程式

---
+++ Question.java
@@ -0,0 +1,42 @@
+import static java.lang.System.out;
+public class Question
+{
+	public static int[] make()
+	{
+		int ansNumber = (int)(Math.random()*8999)+1000;
+		int[] ans = toArray(ansNumber,
+				(new int[Integer.toString(ansNumber).length()]), 0);
+		if(check(ans, 0)){
+			out.println("題目："+ansNumber+"，有重複數字，重出");
+			return make();
+		}
+		//out.println("題目；"+ansNumber+"，沒重複數字，Ya!");
+		return ans;
+	}
+
+	public static int[] toArray(int num, int[] arr, int i)
+	{
+		if(i < arr.length){
+			int digit = num % 10;
+			arr[arr.length - 1 - i] = digit;
+			return toArray((num-digit)/10, arr, i+1);
+		}
+		return arr;
+	}
+
+	public static boolean check(int[] arr, int i)
+	{
+		if(i >= arr.length)
+			return false;
+		return checkInside(arr, i, i+1);//j從i的下一個開始輪
+	}
+
+	public static boolean checkInside(int[] arr, int i, int j)
+	{
+		if(j >= arr.length)
+			return check(arr, i+1);//j輪完了，i往下一格
+		if(arr[i] != arr[j])
+			return checkInside(arr, i, j+1);//固定i，j往下一格
+		return true;
+	}
+}
